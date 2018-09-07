e6c7a9cca03f8c4ef7beec4f20652b24db58bf99
Spencer Fern <USpenSt@PEROOT.COM>
2018-09-07 21:30:39 +0530

    Search student by name modification

--- src/components/form1.1/ViewStudents.js
+++ src/components/form1.1/ViewStudents.js
@@ -6,10 +6,10 @@ class ViewStudents extends Component {
     super(props);
 
     this.state = {
-      data: []
+        data: [],
+        key: ""
     };
     this.onDelete = this.onDelete.bind(this);
-    this.onChange = this.onChange.bind(this);
   }
 
   componentDidMount() {
@@ -38,65 +38,103 @@ class ViewStudents extends Component {
       });
   }
 
-  render() {
-    const displayStudent = this.state.data.map(item => {
-      return (
-        <tr key={item._id}>
-          <td key={item._id}>{item.ITNo}</td>
-          <td key={item._id}>{item.name}</td>
-          <td key={item._id}>{item.email}</td>
-          <td key={item._id}>{item.semester}</td>
-          <td key={item._id}>{item.year}</td>
-          <td key={item._id}>{item.cgpa}</td>
+    onSearchChange(event){
+        event.preventDefault();
+        event.stopPropagation();
+        this.setState({key:event.target.value });
+        //this.searchKey = event.target.value;
+    }
 
-          <td>
-            <div className="btn-group">
-              <a
-                id={item._id}
-                value={item._id}
-                className="btn btn-danger"
-                onClick={this.onDelete}
-              >
-                Delete
-              </a>
+    render() {
+        var displayStudent = this.state.data.map(item => {
+            let searchkey = new RegExp(this.state.key, "i");
+            console.log(searchkey);
+            if(this.state.key === ""){
+                console.log("No search key");
+                return (
+                    <tr key={item._id}>
+                        <td key={item._id}>{item.ITNo}</td>
+                        <td key={item._id}>{item.name}</td>
+                        <td key={item._id}>{item.email}</td>
+                        <td key={item._id}>{item.semester}</td>
+                        <td key={item._id}>{item.year}</td>
+                        <td key={item._id}>{item.cgpa}</td>
+
+                        <td>
+                            <div className="btn-group">
+                                <a
+                                    id={item._id}
+                                    value={item._id}
+                                    className="btn btn-danger"
+                                    onClick={this.onDelete}
+                                >
+                                    Delete
+                                </a>
+                            </div>
+                        </td>
+                    </tr>
+                );
+            } else if(item.ITNo.match(searchkey)){
+                console.log("Search key included");
+                return (
+                    <tr key={item._id}>
+                        <td key={item._id}>{item.ITNo}</td>
+                        <td key={item._id}>{item.name}</td>
+                        <td key={item._id}>{item.email}</td>
+                        <td key={item._id}>{item.semester}</td>
+                        <td key={item._id}>{item.year}</td>
+                        <td key={item._id}>{item.cgpa}</td>
+
+                        <td>
+                            <div className="btn-group">
+                                <a
+                                    id={item._id}
+                                    value={item._id}
+                                    className="btn btn-danger"
+                                    onClick={this.onDelete}
+                                >
+                                    Delete
+                                </a>
+                            </div>
+                        </td>
+                    </tr>
+                );
+            }
+
+        });
+
+        return (
+            <div className="landing-inner">
+                <div className="row">
+                    <div className="col-lg-12">
+                        <section className="panel">
+                            <h1 className="display-4 text-left">FORM1.1 Filled Students</h1>
+                            <FormatedTextField
+                                placeholder="Filter students by ID,Name,Email,Semester,Year or GPA"
+                                name="ITNo"
+                                value={this.state.ITNo}
+                                onChange={event => this.onSearchChange(event)}
+                                info="Search"
+                            />
+                            <table className="table table-striped table-advance table-hover">
+                                <tbody>
+                                <tr>
+                                    <th>Student ID</th>
+                                    <th>Name</th>
+                                    <th>Email</th>
+                                    <th>Semester</th>
+                                    <th>year</th>
+                                    <th>GPA</th>
+                                    <th>Action</th>
+                                </tr>
+                                {displayStudent}
+                                </tbody>
+                            </table>
+                        </section>
+                    </div>
+                </div>
             </div>
-          </td>
-        </tr>
-      );
-    });
-
-    return (
-      <div className="landing-inner">
-        <div className="row">
-          <div className="col-lg-12">
-            <section className="panel">
-              <h1 className="display-4 text-left">FORM1.1 Filled Students</h1>
-              <FormatedTextField
-                placeholder="Filter students by ID,Name,Email,Semester,Year or GPA"
-                name="ITNo"
-                value={this.state.ITNo}
-                onChange={this.onChange}
-                info="Search"
-              />
-              <table className="table table-striped table-advance table-hover">
-                <tbody>
-                  <tr>
-                    <th>Student ID</th>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Semester</th>
-                    <th>year</th>
-                    <th>GPA</th>
-                    <th>Action</th>
-                  </tr>
-                  {displayStudent}
-                </tbody>
-              </table>
-            </section>
-          </div>
-        </div>
-      </div>
-    );
-  }
+        );
+    }
 }
 export default ViewStudents;
